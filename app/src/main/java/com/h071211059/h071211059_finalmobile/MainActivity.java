3d51c5883d7d2dd1661bb77c95a57199b05825ba
erwinarif31 <muherwinarif31@gmail.com>
2023-06-10 14:09:05 +0800

    feat: navbar

--- app/src/main/java/com/h071211059/h071211059_finalmobile/MainActivity.java
+++ app/src/main/java/com/h071211059/h071211059_finalmobile/MainActivity.java
@@ -1,13 +1,17 @@
 package com.h071211059.h071211059_finalmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 import com.h071211059.h071211059_finalmobile.databinding.ActivityMainBinding;
+import com.h071211059.h071211059_finalmobile.fragment.FavoriteFragment;
 import com.h071211059.h071211059_finalmobile.fragment.MovieFragment;
+import com.h071211059.h071211059_finalmobile.fragment.TvFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -21,5 +25,52 @@ public class MainActivity extends AppCompatActivity {
 
        fragmentManager = getSupportFragmentManager();
        MovieFragment movieFragment = MovieFragment.getInstance();
+
+       Fragment fragment = fragmentManager.findFragmentByTag(MovieFragment.class.getSimpleName());
+
+       if (!(fragment instanceof MovieFragment)) {
+           binding.ivMovie.setImageResource(R.drawable.ic_movie);
+           fragmentManager
+                   .beginTransaction()
+                   .add(R.id.fl_container, movieFragment,
+                           MovieFragment.class.getSimpleName())
+                   .commit();
+       }
+
+       binding.llMovie.setOnClickListener(v -> switchFragment(MovieFragment.getInstance()));
+       binding.llTvShow.setOnClickListener(v -> switchFragment(TvFragment.getInstance()));
+       binding.llFavorite.setOnClickListener(v -> switchFragment(FavoriteFragment.getInstance()));
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        binding.ivMovie.setImageResource(R.drawable.ic_movie_unfilled);
+        binding.ivTvShow.setImageResource(R.drawable.ic_tv_unfilled);
+        binding.ivFavorite.setImageResource(R.drawable.ic_favorite_unfilled);
+
+        switch (fragment.getClass().getSimpleName()) {
+            case "MovieFragment":
+                binding.ivMovie.setImageResource(R.drawable.ic_movie);
+                break;
+            case "TvFragment":
+                binding.ivTvShow.setImageResource(R.drawable.ic_tv);
+                break;
+            case "FavoriteFragment":
+                binding.ivFavorite.setImageResource(R.drawable.ic_favorite);
+                break;
+        }
+
+        if (fragment instanceof MovieFragment) {
+            transaction
+                    .replace(R.id.fl_container, fragment,
+                            MovieFragment.class.getSimpleName())
+                    .commit();
+        } else {
+            transaction
+                    .replace(R.id.fl_container, fragment,
+                            MovieFragment.class.getSimpleName())
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
